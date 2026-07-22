869974c577ce7eb7b12acf6ac8a8dbf5af5e43a3
buleoptimist <buleoptimist@users.noreply.github.com>
2026-07-22 11:19:45 +0900

    Auto deploy 2026-07-22 11:19:45

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'es90-sales-app-v67';
-const APP_SHELL = ['./', './index.html', './app.html', './manifest.webmanifest', './icons/es90-icon.svg', './assets/es90-login-cover-lights-on-v3-led.png', './assets/es90-headlight-shape-mask-v1.png'];
+const CACHE_NAME = 'es90-sales-app-v68';
+const APP_SHELL = ['./', './index.html', './app.html', './manifest.webmanifest', './icons/es90-icon.svg', './assets/es90-login-cover-lights-on-v3-led.png', './assets/es90-headlight-shape-mask-v1.png', './assets/es90-login-road-safe-area-v1.jpg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));

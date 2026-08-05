4a37c478f1d1b88c442ea4a31859df96a9328e53
buleoptimist <buleoptimist@users.noreply.github.com>
2026-08-06 02:02:45 +0900

    Auto deploy 2026-08-06 02:02:45

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'es90-sales-app-v218';
+const CACHE_NAME = 'es90-sales-app-v219';
 const APP_SHELL = ['./', './index.html', './app.html', './charger.html', './subsidy.html', './subsidy-data.json', './administrative-dongs.json', './administrative-centers.json', './manifest.webmanifest', './icons/es90-icon.svg', './assets/charger-marker-350kw-v1.png', './assets/es90-login-cover-lights-on-v3-led.png', './assets/es90-headlight-shape-mask-v1.png', './assets/es90-trim-plus.png', './assets/es90-trim-ultra.png', './assets/es90-trim-performance-ultra.png', './assets/dolby-atmos.png', './assets/es90-bnw-dolby-atmos-thumb.png'];
 
 self.addEventListener('install', event => {
